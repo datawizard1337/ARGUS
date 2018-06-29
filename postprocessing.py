6488fdc062ce33d968f02ee567543b7882de65ca
Geomancer <39303881+datawizard1337@users.noreply.github.com>
2018-06-29 13:26:06 +0200

    Fixed postprocessing

--- postprocessing.py
+++ postprocessing.py
@@ -55,13 +55,10 @@ elif config.get('spider-settings', 'spider') == "link":
                 break
             if line == "\n":
                 continue
-            try:
-                line = line.split("\t")
-                dl_slot = line[2]
-                if dl_slot not in all_internal_links:
-                    all_internal_links.append(dl_slot)
-            except:
-                continue
+            line = line.split("\t")
+            dl_slot = line[2]
+            if dl_slot not in all_internal_links:
+                all_internal_links.append(dl_slot)
         f.close()
     #write outputfile
     c=0
@@ -84,40 +81,37 @@ elif config.get('spider-settings', 'spider') == "link":
                 break
             if line == "\n":
                 continue
-            try:
-                line = line.split("\t")
-                links = line[4].split(",")
-                #lists to collect links which are either from initial/internal population or from external websites
-                #first item is self --> easier to import into analysis software later on
-                links_internal = [line[2]]
-                links_external = [line[2]]
-                for link in links:
-                    if link == "":
-                        continue
-                    #if the link is from the internal population...
-                    if link in all_internal_links:
-                        #...and has not been saved yet, add to internal links
-                        if link not in links_internal:
-                            links_internal.append(link)
-                    #...if not from internal population add to external population
-                    else:
-                        links_external.append(link)
-                        
-                #create output string
-                if len(links_internal) == 1:
-                    links_internal = ""
+            line = line.split("\t")
+            links = line[4].split(",")
+            #lists to collect links which are either from initial/internal population or from external websites
+            #first item is self --> easier to import into analysis software later on
+            links_internal = [line[2]]
+            links_external = [line[2]]
+            for link in links:
+                if link == "":
+                    continue
+                #if the link is from the internal population...
+                if link in all_internal_links:
+                    #...and has not been saved yet, add to internal links
+                    if link not in links_internal:
+                        links_internal.append(link)
+                #...if not from internal population add to external population
                 else:
-                    links_internal = ",".join(links_internal)   
-                if len(links_external) == 1:
-                    links_external = ""
-                else:
-                    links_external = ",".join(links_external)
+                    links_external.append(link)
+                    
+            #create output string
+            if len(links_internal) == 1:
+                links_internal = ""
+            else:
+                links_internal = ",".join(links_internal)   
+            if len(links_external) == 1:
+                links_external = ""
+            else:
+                links_external = ",".join(links_external)
 
-                #write to output file                    
-                output_line = line[0] + "\t" + line[1] + "\t" + line[2] + "\t" + line[3] + "\t" + links_internal + "\t" + links_external + "\t" + line[5] + "\t"  + line[6] + "\t" + line[7]
-                merged_file.write(output_line)
-            except:
-                continue
+            #write to output file                    
+            output_line = line[0] + "\t" + line[1] + "\t" + line[2] + "\t" + line[3] + "\t" + links_internal + "\t" + links_external + "\t" + line[5] + "\t"  + line[6] + "\t" + line[7]
+            merged_file.write(output_line)
         f.close()        
 merged_file.close()    
 
