8446fc007d378af0113c55233344a7c3800489eb
Geomancer <39303881+datawizard1337@users.noreply.github.com>
2018-08-23 08:43:19 +0200

    Try/Exception added
    In very rare cases (1 in millions), certain special characters cause the postprocessing to fail when indexing on lines. Added try/exception to catch that. Also added tiny error report.

--- postprocessing.py
+++ postprocessing.py
@@ -43,22 +43,26 @@ if config.get('spider-settings', 'spider') == "text":
         
 #linkspider postprocessing
 elif config.get('spider-settings', 'spider') == "link":
-    
+    errors = 0
     #generate list of all internal (given in user url list) domains
     all_internal_links = []
     for fn in output_files:
         f = open(fn, encoding="utf-8")
         f.readline()
         while 1:
-            line = f.readline()
-            if not line:
-                break
-            if line == "\n":
+            try:
+                line = f.readline()
+                if not line:
+                    break
+                if line == "\n":
+                    continue
+                line = line.split("\t")
+                dl_slot = line[2]
+                if dl_slot not in all_internal_links:
+                    all_internal_links.append(dl_slot)
+            except:
+                errors += 1
                 continue
-            line = line.split("\t")
-            dl_slot = line[2]
-            if dl_slot not in all_internal_links:
-                all_internal_links.append(dl_slot)
         f.close()
     #write outputfile
     c=0
@@ -76,47 +80,51 @@ elif config.get('spider-settings', 'spider') == "link":
        
         #iterate lines and extract links, divide into internal and external links, write to output file
         while 1:
-            line = f.readline()
-            if not line:
-                break
-            if line == "\n":
-                continue
-            line = line.split("\t")
-            links = line[4].split(",")
-            #lists to collect links which are either from initial/internal population or from external websites
-            #first item is self --> easier to import into analysis software later on
-            links_internal = [line[2]]
-            links_external = [line[2]]
-            for link in links:
-                if link == "":
+            try:
+                line = f.readline()
+                if not line:
+                    break
+                if line == "\n":
                     continue
-                #if the link is from the internal population...
-                if link in all_internal_links:
-                    #...and has not been saved yet, add to internal links
-                    if link not in links_internal:
-                        links_internal.append(link)
-                #...if not from internal population add to external population
+                line = line.split("\t")
+                links = line[4].split(",")
+                #lists to collect links which are either from initial/internal population or from external websites
+                #first item is self --> easier to import into analysis software later on
+                links_internal = [line[2]]
+                links_external = [line[2]]
+                for link in links:
+                    if link == "":
+                        continue
+                    #if the link is from the internal population...
+                    if link in all_internal_links:
+                        #...and has not been saved yet, add to internal links
+                        if link not in links_internal:
+                            links_internal.append(link)
+                    #...if not from internal population add to external population
+                    else:
+                        links_external.append(link)
+                        
+                #create output string
+                if len(links_internal) == 1:
+                    links_internal = ""
                 else:
-                    links_external.append(link)
-                    
-            #create output string
-            if len(links_internal) == 1:
-                links_internal = ""
-            else:
-                links_internal = ",".join(links_internal)   
-            if len(links_external) == 1:
-                links_external = ""
-            else:
-                links_external = ",".join(links_external)
-
-            #write to output file                    
-            output_line = line[0] + "\t" + line[1] + "\t" + line[2] + "\t" + line[3] + "\t" + links_internal + "\t" + links_external + "\t" + line[5] + "\t"  + line[6] + "\t" + line[7]
-            merged_file.write(output_line)
+                    links_internal = ",".join(links_internal)   
+                if len(links_external) == 1:
+                    links_external = ""
+                else:
+                    links_external = ",".join(links_external)
+    
+                #write to output file                    
+                output_line = line[0] + "\t" + line[1] + "\t" + line[2] + "\t" + line[3] + "\t" + links_internal + "\t" + links_external + "\t" + line[5] + "\t"  + line[6] + "\t" + line[7]
+                merged_file.write(output_line)
+            except:
+                errors += 1
+                continue
         f.close()        
 merged_file.close()    
 
-print("Merging done. Deleting leftovers...")
-time.sleep(2)
+print("Merging done. Skipped {} websites because of formatting errors. Deleting leftovers...".format(errors))
+time.sleep(5)
 
 #delete chunks
 chunk_files = [os.getcwd() + "\\chunks\\" + f for f in os.listdir(os.getcwd() + "\\chunks") if f.split("_")[0] == "url"]
